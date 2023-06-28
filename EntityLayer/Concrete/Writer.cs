b8e9ebdec996d247a7ff0b4ef2e1a77a2f5ded0d
Cloutout <75390041+Cloutout@users.noreply.github.com>
2023-06-28 14:13:12 +0300

    entity layerda classlar olusturuldu

---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,13 @@
+﻿namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterId { get; set; }
+        public string WriterName { get; set; }
+        public string WriterSurname { get; set; }
+        public string WriterImage { get; set; }
+        public string WriterMail { get; set; }
+        public string WriterPassword { get; set; }
+        
+    }
+}
