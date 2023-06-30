e8c9b7724aabd12a5bb973105a16c3b0905c7d76
Cloutout <75390041+Cloutout@users.noreply.github.com>
2023-06-30 10:32:04 +0300

    DbContext class i ve WebConfig ayarlandi

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+        //sql tarafinda olusan tablolar buradan yapilir ve tablo isimlerinin sonu hep s takisi ile biter
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+    }
+}
